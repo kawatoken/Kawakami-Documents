b792a07018cb18baafa5f0224172fcc34511fffa
Michael <contact@openwavedigital.com>
2021-11-17 18:11:14 -0500

    fixing nuxt generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,12 @@
 import theme from '@nuxt/content-theme-docs'
 
-export default {
+export default theme({
   generate: {
         dir: 'docs', // gh_pages/ instead of dist/
     subFolders: false // HTML files are generated according to the route path
   },
-  theme: {
   docs: {
     primaryColor: '#E24F55'
   }
-}
-}
+})
 
