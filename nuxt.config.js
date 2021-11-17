ce32def1f8a5e747cd89bf4784c3e2c59f9437b6
Michael <contact@openwavedigital.com>
2021-11-17 18:00:55 -0500

    renaming dist folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,14 @@
 import theme from '@nuxt/content-theme-docs'
 
-export default theme({
+export default {
+  generate: {
+        dir: 'docs', // gh_pages/ instead of dist/
+    subFolders: false // HTML files are generated according to the route path
+  },
+  theme: {
   docs: {
     primaryColor: '#E24F55'
   }
-})
+}
+}
+
