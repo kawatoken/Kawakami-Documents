834c2896028751482a323bdca0b8c65626a02eac
Michael <contact@openwavedigital.com>
2021-11-17 18:26:02 -0500

    fixing routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,7 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   generate: {
         dir: 'docs', // gh_pages/ instead of dist/
+        routes: ['/']
 //    subFolders: false // HTML files are generated according to the route path
   },
   docs: {
