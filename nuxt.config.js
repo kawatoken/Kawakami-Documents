adbd516ff85ac535984a1bfe33187359aa1e0bff
Michael <contact@openwavedigital.com>
2021-11-17 18:49:49 -0500

    fixing routes 7

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default theme({
         dir: 'docs', // gh_pages/ instead of dist/
 //    subFolders: false // HTML files are generated according to the route path
   },
+  router: {
+    base: '/KawaPack.github.io'
+
+},
   docs: {
     primaryColor: '#E24F55'
   }
