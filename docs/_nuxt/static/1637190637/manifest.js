3c31d64eb18c7276058993c8c0272d4683bd9a49
Michael <contact@openwavedigital.com>
2021-11-17 18:13:36 -0500

    fixing nuxt generate again

--- docs/_nuxt/static/1637190637/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002FCommunityBlog","\u002FYearUpdate","\u002FShillingGuide","\u002FOfficialLinks"]})
---
+++ docs/_nuxt/static/1637190762/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002FCommunityBlog","\u002FYearUpdate","\u002FOfficialLinks","\u002FShillingGuide"]})
