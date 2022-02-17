b5de76aca9a6c719f8796f0f792024d0f35cea25
Michael Forest <openwavedigital@gmail.com>
2022-02-17 09:01:48 -0500

    v1.07

--- docs/_nuxt/static/1645106279/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002FKawa","\u002FxKawa","\u002FOfficialLinks","\u002FBSC-Bridge","\u002FKawaDAO","\u002Fkawafarm-staking","\u002Fusing-BSC-bridge-kawa","\u002Fbuying-kawa-uniswap"]})
