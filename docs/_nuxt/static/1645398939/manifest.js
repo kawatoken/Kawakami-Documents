7715436658871f17e801e031276817e69ee33b1f
Michael Forest <openwavedigital@gmail.com>
2022-02-20 18:19:21 -0500

    updated light logo

--- docs/_nuxt/static/1645398939/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002FKawa-v3","\u002FBSC-Bridge","\u002Fkawafarm-staking","\u002Fbuying-kawa-uniswap","\u002FKawa-v2","\u002FKawaDAO","\u002Fusing-BSC-bridge-kawa","\u002FOfficialLinks","\u002FxKawa","\u002FKawa-v1"]})
---
+++ docs/_nuxt/static/1645399120/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002FOfficialLinks","\u002FKawa-v1","\u002FKawa-v2","\u002Fkawafarm-staking","\u002Fusing-BSC-bridge-kawa","\u002FBSC-Bridge","\u002FKawaDAO","\u002Fbuying-kawa-uniswap","\u002FKawa-v3","\u002FxKawa"]})
