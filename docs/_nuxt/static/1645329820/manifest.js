f2c3f6a68ce7176bf3b150776fad52b9edf7e00c
Michael Forest <openwavedigital@gmail.com>
2022-02-19 23:04:04 -0500

    update cname file

---
+++ docs/_nuxt/static/1645329820/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Freleases","\u002FKawaDAO","\u002FKawa-v3","\u002Fkawafarm-staking","\u002FOfficialLinks","\u002Fbuying-kawa-uniswap","\u002FBSC-Bridge","\u002Fusing-BSC-bridge-kawa","\u002FKawa-v1","\u002FxKawa","\u002FKawa-v2"]})
