8721a0555a22d9578d897506f4ecbec50c257cb6
Michael Synan <mforest@protonmail.com>
2022-03-03 18:25:55 -0500

    default dark mode

--- docs/_nuxt/static/1646335404/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Freleases","\u002F","\u002FxKawa","\u002FKawaDAO","\u002Fbuying-kawa-uniswap","\u002FBSC-Bridge","\u002Fkawafarm-staking","\u002FImportant-Links","\u002FKawa"]})
